590784c4b8e3ae9972e62a44b173ca1de3f47674
codingnuclei <codingnuclei@gmail.com>
2024-05-13 10:30:17 +0100

    ci: enable publish and release workflows

---
+++ scripts/release.js
@@ -0,0 +1,68 @@
+// @ts-check
+const { releaseVersion, releaseChangelog } = require('nx/release');
+const yargs = require('yargs');
+
+(async () => {
+  try {
+    const options = await yargs
+      // @ts-expect-error - don't use the default meaning of version in yargs
+      .version(false)
+      .option('version', {
+        type: 'string',
+      })
+      .option('dryRun', {
+        alias: 'd',
+        type: 'boolean',
+        default: true,
+      })
+      .option('verbose', {
+        type: 'boolean',
+        default: false,
+      })
+      .parseAsync();
+
+    if (!options.dryRun) {
+      if (!process.env.GH_TOKEN && !process.env.GITHUB_TOKEN) {
+        throw new Error(
+          `GH_TOKEN or GITHUB_TOKEN environment variable must be set in order to run a real release`
+        );
+      }
+    }
+
+    console.log();
+    console.info(`********* Release Options **********`);
+    console.info(
+      `version   : ${options.version ?? 'use conventional commits'}`
+    );
+    console.info(
+      `dryRun    : ${options.dryRun} ${options.dryRun ? '😅' : '🚨🚨🚨'}`
+    );
+    console.info(`verbose   : ${options.verbose}`);
+    console.log();
+
+    const { workspaceVersion, projectsVersionData } = await releaseVersion({
+      specifier: options.version,
+      dryRun: options.dryRun,
+      verbose: options.verbose,
+    });
+
+    // This will create a release on GitHub, which will act as a trigger for the publish.yml workflow
+    const status = await releaseChangelog({
+      version: workspaceVersion,
+      versionData: projectsVersionData,
+      dryRun: options.dryRun,
+      verbose: options.verbose,
+    });
+
+    if (!options.dryRun) {
+      console.log(
+        'Check GitHub: https://github.com/kitchenshelf/serverless-rspack/actions/workflows/publish.yml'
+      );
+    }
+
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
